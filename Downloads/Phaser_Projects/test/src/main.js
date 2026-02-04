e31eca12aabdc5126cff98bebfd84fbdf0ef205b
BroodMotherPlayer <89310770+BroodMotherPlayer@users.noreply.github.com>
2026-02-04 23:52:39 +0800

    Update main.js

--- Downloads/Phaser_Projects/test/src/main.js
+++ Downloads/Phaser_Projects/test/src/main.js
@@ -12,7 +12,7 @@ class MainMenu extends Phaser.Scene {
     super('MainMenu');
   }
   preload() {
-    this.load.image('background', './sprites/background.jpg');
+    this.load.image('background', '/sprites/background.jpg');
   }
   create() {
     const {width,height}= this.scale
@@ -37,11 +37,11 @@ class GamePlay extends Phaser.Scene {
     super('GamePlay');
   }
   preload() {
-    this.load.image('background', './sprites/background.jpg');
-    this.load.image('hitZone', './sprites/hitZone.png');
-    this.load.image('hitZoneShadow', './sprites/note.png');
-    this.load.image('note', './sprites/note.png');
-    this.load.audio('bgm',"./audio/background_track.mp3")
+    this.load.image('background', '/sprites/background.jpg');
+    this.load.image('hitZone', '/sprites/hitZone.png');
+    this.load.image('hitZoneShadow', '/sprites/note.png');
+    this.load.image('note', '/sprites/note.png');
+    this.load.audio('bgm',"/audio/background_track.mp3")
 
   }
   create() {
